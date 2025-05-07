58c18d6d7073e94f16268cc6bddc780e2e30a616
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-05-07 14:04:05 +0000

    hi

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AdminUploadForm from './components/AdminUploadForm';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <AdminUploadForm />
+        </React.StrictMode>
+        );
+        
